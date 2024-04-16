4d45c1e8b903d28590de3c3492b23e23df43aa0e
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-16 16:05:19 +0530

    Add files via upload

---
+++ bankofindia.java
@@ -0,0 +1,21 @@
+interface RBI{
+	void Loan();
+}
+class hdfc implements RBI{
+	public void Loan() {
+		System.out.println("Loan verfied by Hdfc");
+	}
+}
+public class bankofindia implements RBI{
+public void Loan(){
+	System.out.println("Loan Verified by Boi");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+bankofindia user1=new bankofindia();
+user1.Loan();
+hdfc user2=new hdfc();
+user2.Loan();
+	}
+
+}
