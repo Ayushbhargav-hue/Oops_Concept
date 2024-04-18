73702e60910b70e339b42b3632f1328c1830d5c0
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-18 16:14:28 +0530

    Create lengthmethodstring.java

---
+++ lengthmethodstring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class lengthmethodstring
+{
+	public static void main(String[] args) {
+	
+		String s1="railworldindoremadhyapradesh";
+	System.out.println(s1.length());	
+	
+	}
+}
