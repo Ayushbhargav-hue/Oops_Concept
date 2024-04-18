c9d9b75f474506e9c911d2cb4f9e1fac3d5dbdfa
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-18 16:12:51 +0530

    Create substringmethodexample.java

---
+++ substringmethodexample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class substringmethodexample
+{
+	public static void main(String[] args) {
+	
+		String s1="railworldindoremadhyapradesh";
+	System.out.println(s1.substring(0,9));	
+	
+	}
+}
