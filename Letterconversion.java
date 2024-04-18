dd353879eb606e239858e2ee899c930b98a8b663
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-18 15:59:16 +0530

    Create Letterconversion.java

---
+++ Letterconversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Letterconversion
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+	System.out.println(s1.toUpperCase()+" Convert to upper case");
+	System.out.println(s2.toLowerCase()+" Convert to lower case");
+	}
+}
