d90d31672816b039841c7f6523d48bf9a29fa00e
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-18 16:18:11 +0530

    Create charatmethodstring.java

---
+++ charatmethodstring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class charatmethodstring
+{
+	public static void main(String[] args) {
+	
+		String s1="railworldindoremadhyapradesh";
+	System.out.println(s1.charAt(6));	
+	System.out.println(s1.indexOf("l"));
+	}
+}
