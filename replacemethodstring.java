c6e89fd90f2039562acd20274059133a67fb18e4
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-18 16:20:11 +0530

    Create replacemethodstring.java

---
+++ replacemethodstring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class replacemethodstring
+{
+	public static void main(String[] args) {
+	
+		String s1="10101010101";
+	System.out.println(s1.replace('0','1'));	
+
+	}
+}
