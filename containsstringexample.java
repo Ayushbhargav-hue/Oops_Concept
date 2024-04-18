90aeb6e7c7bea9d92655cc4dbbeffb4ff330ed23
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-18 16:10:04 +0530

    Add files via upload

---
+++ containsstringexample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class containsstringexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+	System.out.println(s1.contains("railworld"));
+	
+	}
+
+}
