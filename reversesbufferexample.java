66746bbf370627c151ad3c1929d24597942d4be5
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-22 22:47:51 +0530

    Create reversesbufferexample.java

---
+++ reversesbufferexample.java
@@ -0,0 +1,9 @@
+//reverse a string using string buffer method
+public class reversesbufferbexample
+{
+	public static void main(String[] args) {
+		StringBuffer s=new StringBuffer("avaJ");
+		s.reverse();
+		System.out.println(s);
+	}
+}
