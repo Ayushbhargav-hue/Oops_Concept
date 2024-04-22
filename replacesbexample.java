fe3e3ff1fcc9e9a60ffd2a4ab7eda5823a09dae8
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-22 15:30:07 +0530

    Create replacesbexample.java

---
+++ replacesbexample.java
@@ -0,0 +1,9 @@
+//replace method in string builder example
+public class replacesbexample
+{
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("Jsda");
+		s.replace(1,3,"av");
+		System.out.println(s);
+	}
+}
