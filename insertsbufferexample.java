f66d43ac43dd5c7c3cd52656df79640a6a3544b7
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-22 22:48:23 +0530

    Update insertsbufferexample.java

--- insertsbufferexample.java
+++ insertsbufferexample.java
@@ -2,7 +2,7 @@
 public class insertsbufferexample
 {
 	public static void main(String[] args) {
-		StringBuilder s=new StringBuilder("Jva");
+		StringBuffer s=new StringBuffer("Jva");
 		s.insert(1,"a");
 		System.out.println(s);
 	}
