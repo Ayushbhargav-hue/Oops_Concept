3b41b10e20990bf33b2f5b1c35bb2390f7dc1485
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-22 15:34:02 +0530

    Create reversesbexample.java

---
+++ reversesbexample.java
@@ -0,0 +1,9 @@
+//reverse a string using string builder method
+public class reversesbexample
+{
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("avaJ");
+		s.reverse();
+		System.out.println(s);
+	}
+}
