b4a792e7cb47d10284a575dea543203f24069605
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-22 15:27:38 +0530

    Create insertsbexample.java

---
+++ insertsbexample.java
@@ -0,0 +1,9 @@
+//stringbuilder insert example
+public class insertsbexample
+{
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("Jva");
+		s.insert(1,"a");
+		System.out.println(s);
+	}
+}
