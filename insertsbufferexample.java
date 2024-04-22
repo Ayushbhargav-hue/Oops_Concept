4833a3cb7adf93a9cfe358dcf74109587266a76a
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-22 22:46:41 +0530

    Create insertsbufferexample.java

---
+++ insertsbufferexample.java
@@ -0,0 +1,9 @@
+//stringbuffer insert example
+public class insertsbufferexample
+{
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("Jva");
+		s.insert(1,"a");
+		System.out.println(s);
+	}
+}
