724371eca0583125c473086dac63b8b3c3d60944
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-04-25 21:53:42 +0530

    Update and rename Main2.java to Thread1.java

--- Thread1.java
+++ Thread1.java
@@ -1,15 +1,15 @@
-class Thread1 extends Thread{
+class Thread2 extends Thread{
     public void run(){
         System.out.println("Running Thread1 class");
     }
 }
-public class Main implements Runnable
+public class Thread1 implements Runnable
 { public void run(){
     System.out.println("Running Main class thread");
 }
 	public static void main(String[] args) {
 		
-		Main m=new Main();
+		Thread1 m=new Thread1();
 		Thread t1=new Thread(m);
 		t1.start();
 		Thread1 t2=new Thread1();
