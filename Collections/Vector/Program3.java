c5e8515dca936f0e3974ebf12aa4933daf38c985
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-03 15:58:46 +0530

    Add files via upload

--- Collections/Vector/Program3.java
+++ Collections/Vector/Program3.java
@@ -2,10 +2,11 @@ import java.util.Vector;
 class A{
 	public static void main(String args[]){
 		Vector<String> vec=new Vector<>();
-		System.out.println("Is this vector is empty : "+vec.isEmpty());
+		
 		vec.addElement("ArrayList");
 		vec.addElement("LinkedList");
 		vec.addElement("Stack");
-		System.out.println(vec.removeAll(vec));
+		vec.trimToSize();
+		System.out.println(vec);
 	}
 }
