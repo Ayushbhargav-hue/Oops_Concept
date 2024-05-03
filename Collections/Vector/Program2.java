be81e7dd75ad4a9d3b7acc8f54b67beb7a8bb3e5
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-03 15:33:09 +0530

    Add files via upload

---
+++ Collections/Vector/Program2.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+class A{
+	public static void main(String args[]){
+		Vector<String> vec=new Vector<>();
+		vec.addElement("ArrayList");
+		vec.addElement("LinkedList");
+		vec.addElement("Stack");
+		System.out.println(vec);
+	}
+}
