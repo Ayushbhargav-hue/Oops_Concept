f43ad27331e8860d3c67a7e52dd9302c8fef7b17
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-06 10:12:53 +0530

    Add files via upload

---
+++ Collections/Set/Program2.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+class {
+public static void main(String args[]){
+Set<String> str=new HashSet<>();
+str.add("Bhopal");
+str.add("Indore");
+str.add("Gwalior");
+str.add("Bhopal");
+str.add("Guna");
+ArrayList<String> arr=new ArrayList<>();
+arr.add("Indore");
+arr.add("Raisen");
+arr.add("Sehore");
+str.addAll(arr);
+System.out.println(str);
+}
+}
