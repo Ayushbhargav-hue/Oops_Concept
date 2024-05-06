88ff4117c0e2731211b47d463740ced52928ff01
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-06 10:48:56 +0530

    Create Program4.java

---
+++ Collections/HashMap/Program4.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+public class A
+{
+	public static void main(String[] args) {
+		HashMap<Integer,String> map=new HashMap<>();
+		map.put(1,"Bhopal");
+		map.put(2,"Gwalior");
+		HashMap<Integer,String> map1=new HashMap<>();
+		map1.put(6,"Indore");
+		map1.put(3,"Raipur");
+		map.putAll(map1);
+		map.replace(3,"Raipur","Guna");
+		System.out.println(map);
+	}
+}
