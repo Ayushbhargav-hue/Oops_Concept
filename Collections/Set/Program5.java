277c7b79f42c5ccd3aa500d29c6963e720f8cc76
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-06 10:36:23 +0530

    Create Program5.java

---
+++ Collections/Set/Program5.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+public class A
+{
+	public static void main(String[] args) {
+			Set<String> str=new HashSet<>();
+str.add("Bhopal");
+str.add("Indore");
+str.add("Gwalior");
+str.add("Bhopal");
+str.add("Guna");
+ArrayList<String> arr=new ArrayList<>();
+arr.add("Indore");
+arr.add("Raisen");
+arr.add("Sehore");
+str.clear();
+System.out.println(str);
+	}
+}
