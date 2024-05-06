79038cd8ad08e71deed92c1cd39f4316132cd2c5
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-06 10:06:09 +0530

    Update Program1.java

--- Collections/Set/Program1.java
+++ Collections/Set/Program1.java
@@ -1,5 +1,12 @@
 import java.util.Set;
+import java.util.HashSet;
 class A{
 public static void main (String args[]){
+  Set<String> str=new HashSet<>();
+  str.add("Railworld");
+  str.add("Indore");
+  str.add("Raipur");
+  str.add("Hyderabad");
+  System.out.println(str);
 }
 }
