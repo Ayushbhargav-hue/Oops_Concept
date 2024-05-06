0fd4f7a932cc30998ce1fb3f909ebaee30e4f78b
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-06 10:40:16 +0530

    Update Program1.java

--- Collections/HashMap/Program1.java
+++ Collections/HashMap/Program1.java
@@ -1,5 +1,10 @@
-import java.util.Set;
-class A{
-public static void main (String args[]){
-}
+import java.util.HashMap;
+public class A
+{
+	public static void main(String[] args) {
+		HashMap<Integer,String> map=new HashMap<>();
+		map.put(1,"Bhopal");
+		map.put(2,"Gwalior");
+		System.out.println(map);
+	}
 }
