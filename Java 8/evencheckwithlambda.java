1a120d9f2ce968f3255d0e157ee77ba9e4294f27
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-07 17:06:47 +0530

    Update evencheckwithlambda.java

--- Java 8/evencheckwithlambda.java
+++ Java 8/evencheckwithlambda.java
@@ -5,7 +5,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		A user=b->b%2==0;
-		if(user.chech(2))System.out.println("Even");
+		if(user.check(2))System.out.println("Even");
 		else System.out.println("Odd");
 		}
 		
