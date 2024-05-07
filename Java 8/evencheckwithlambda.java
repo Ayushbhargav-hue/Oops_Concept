845c93b03864578440d65be2d8ed70611faec63f
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-07 17:10:20 +0530

    Update evencheckwithlambda.java

--- Java 8/evencheckwithlambda.java
+++ Java 8/evencheckwithlambda.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
 interface A {
         boolean check(int num);
     }
 public class Main
 {
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int c=sc.nextInt();
 		A user=b->b%2==0;
-		if(user.check(2))System.out.println("Even");
+		if(user.check(c))System.out.println("Even");
 		else System.out.println("Odd");
 		}
 		
