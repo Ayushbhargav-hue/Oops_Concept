1f915c00fab99ef5533b4115d3c74ea46ad0c319
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-09 12:02:10 +0530

    Update Streammfilterexammple.java

--- Java 8/Streammfilterexammple.java
+++ Java 8/Streammfilterexammple.java
@@ -1,10 +1,11 @@
-public class lambdawiththread{
-	 
-    public static void main(String[] args) {
-       Runnable runnable=()->System.out.println("Lambda experission with thread");
-       Thread thread =new Thread(runnable);
-       thread.start();
-    }
+import java.util.List;
 
-  
+public class Streammfilterexammple {
+
+	public static void main(String[] args) {
+		List<Integer> list=List.of(1,2,3,5,6,1,2,1);
+		list.stream().filter(i->i%2==0).forEach(System.out::println);
+
+	}
+
 }
