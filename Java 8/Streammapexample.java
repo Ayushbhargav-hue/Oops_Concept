7f5311e93f5526e366fa25e2d37e06317c9581eb
Ayush Bhargav <76631593+Ayushbhargav-hue@users.noreply.github.com>
2024-05-09 12:07:21 +0530

    Update Streammapexample.java

--- Java 8/Streammapexample.java
+++ Java 8/Streammapexample.java
@@ -1,10 +1,11 @@
-public class lambdawiththread{
-	 
-    public static void main(String[] args) {
-       Runnable runnable=()->System.out.println("Lambda experission with thread");
-       Thread thread =new Thread(runnable);
-       thread.start();
-    }
+import java.util.Arrays;
+import java.util.List;
+public class Streammapexample {
 
-  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+List<Integer> list=Arrays.asList(2,4,6,8,9);
+list.stream().map(i->i*i).forEach(System.out::println);
+	}
+
 }
